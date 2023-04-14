98ffde6cc911daba1d06dcdf52b6902ec4780aaf
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-04-14 15:11:41 +0300

    Completed

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,36 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+
+#   get '/' do
+#     "Hello 👋" :username"
+#     "Hi! :username"
+#   end
+# end
+
+  get '/messages' do
+     msgs = Message.all
+     msgs.to_json
+  end
+ 
+  post '/messages' do
+    new_msg = Message.create(
+       body: params[:body],
+       username: params[:username],)
+    new_msg.to_json
+  end
   
+  patch '/messages/:id' do
+    updated_msg = Message.find(params[:id])
+    updated_msg.update(
+      body: params[:body])
+    updated_msg.to_json
+  end
+
+  delete '/messages/:id' do
+    deleted_msg = Message.find(params[:id])
+    deleted_msg.destroy
+    deleted_msg.to_json
+  end
+
 end
